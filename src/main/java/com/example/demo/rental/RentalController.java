be3ea838c31d9cc3da9deb1f62879d7b2e852ce1
mindotori <148763440+mindotori@users.noreply.github.com>
2024-08-25 17:56:30 +0900

    main 수정/ rentalDetails연결

--- src/main/java/com/example/demo/rental/RentalController.java
+++ src/main/java/com/example/demo/rental/RentalController.java
@@ -10,4 +10,18 @@ public class RentalController {
     public String rental() {
         return "rental/rental"; // rental 폴더 안의 rental.html 파일
     }
+    @GetMapping("/rentalDetails1")
+    public String rentalDetails1() {
+        return "rental/rentalDetails1";  // rentalDetails1.html로 이동
+    }
+
+    @GetMapping("/rentalDetails2")
+    public String rentalDetails2() {
+        return "rental/rentalDetails2";  // rentalDetails2.html로 이동
+    }
+
+    @GetMapping("/rentalDetails3")
+    public String rentalDetails3() {
+        return "rental/rentalDetails3";  // rentalDetails3.html로 이동
+    }
 }
